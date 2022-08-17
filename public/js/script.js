292716822c728b61883304aeb4bb4fb63ca9e0dd
TMarine <marine.tumanyan@gmail.com>
2022-08-18 02:33:02 +0400

    footer styles

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+	 // Owl Carousel
+$(function () {
+	 var owl = $(".owl-carousel");
+	 owl.owlCarousel({
+		items: 4.4,
+		margin: 0,
+		loop: true,
+		autoplay: true,
+		autoPlay: 500,
+		stagePaddingLeft: 100,
+		nav: true
+	 });
+  });
+  	 // responsive menu
+  $(document).ready(function () {
+	$('.sidebarbtn').click(function () {
+	  $('#mySidenav, body').toggleClass('active');
+	});
+	$('.closebtn').click(function () {
+	  $('#mySidenav, body').removeClass('active');
+	});
+  });
+  
